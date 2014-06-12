cc0a78fca1e20a792d537329b129c963ddc7fce5
家俊 周 <zjj@zjjMacBook-Pro.local>
2014-06-12 17:24:26 +0800

    增加图片，增加抖动

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,28 @@
 $(document).ready(function () {
     $('#cnzz_stat_icon_5894820').hide();
 
+    $(this).find('.connect').slideDown("slow");
+
     var ixiaozhidomain = $.cookie('ixiaozhidomain');
     if (ixiaozhidomain == null) {
         $("#domain").val("ixiaozhi.com");
     } else {
         $("#domain").val(ixiaozhidomain);
     }
+
+    $('#icon_blog,#icon_camera_diandian,#icon_camera_lofter,#icon_blog_weibo,#icon_blog_google,#icon_blog_facebook,#icon_blog_twitter').jrumble({
+        speed: 0,
+        x: 0,
+        y: 0,
+        rotation: 5,
+        opacity: true
+    });
+
+    $('#icon_blog,#icon_camera_diandian,#icon_camera_lofter,#icon_blog_weibo,#icon_blog_google,#icon_blog_facebook,#icon_blog_twitter').hover(function () {
+        $(this).trigger('startRumble');
+    }, function () {
+        $(this).trigger('stopRumble');
+    });
 
     $('.page-container form').submit(function () {
         var domainvalue = $('#domain').val();
